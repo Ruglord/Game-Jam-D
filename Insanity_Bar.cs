c4eb3b3eea7edb549358a945feb23c2972a9acb2
jmarce44 <43225948+jmarce44@users.noreply.github.com>
2018-11-03 15:03:05 -0400

    Add files via upload

---
+++ Insanity_Bar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Insanity_Bar : MonoBehaviour {
+
+    public int insanity = 0;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("j"))
+        {
+            insanity = insanity + 10;
+        }
+        if (Input.GetKey("k"))
+        {
+            insanity = insanity + 15;
+        }
+        if (Input.GetKey("l"))
+        {
+            insanity = insanity + 20;
+        }
+
+
+	}
+}
